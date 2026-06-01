2b5d8b22763dfb53f2f467ec960947939b205ca8
Vineet Rathi <vineetrathee9053@gmail.com>
2026-06-01 13:27:48 +0530

    day_1

---
+++ Week_1/Day_1/best_time_to_buy_and_sell.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int mi=prices[0];
+       int ans=0;
+       for(int i=1;i<prices.size();i++) 
+       {
+            ans=max(ans,prices[i]-mi);
+            mi=min(mi,prices[i]);
+       }
+       return ans;
+    }
+};
