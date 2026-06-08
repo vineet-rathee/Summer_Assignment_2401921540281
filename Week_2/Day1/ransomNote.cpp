c958f63fd0fb69bf824701a1f1cd5767828ef6cf
Vineet Rathi <vineetrathee9053@gmail.com>
2026-06-08 14:12:27 +0530

    week-2 day-1

---
+++ Week_2/Day1/ransomNote.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(auto it:ransomNote) mp[it]--;
+        for(auto it:magazine) mp[it]++;
+        for(auto it:mp)
+        {
+            if(it.second<0) return false;
+        }
+        return true;
+    }
+};
