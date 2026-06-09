f26c07b29c66e1cdedfec0c3755f3ff191e56a31
Vineet Rathi <vineetrathee9053@gmail.com>
2026-06-09 12:00:16 +0530

    week-2 day-2

--- Week_2/Day2/all_anagram_in_string.cpp
+++ Week_2/Day2/all_anagram_in_string.cpp
@@ -7,21 +7,21 @@ public:
         if(n>=m)
         {
             vector<int> a(26),b(26);
-        for(auto it:p)
-        {
-            a[it-'a']++;
-        }
-        for(int i=0;i<m;i++)
-        {
-            b[s[i]-'a']++;
-        }
-        if(a==b) ans.push_back(0);
-        for(int i=m;i<n;i++)
-        {
-            b[s[i]-'a']++;
-            b[s[i-m]-'a']--;
-            if(a==b) ans.push_back(i-m+1);
-        }
+            for(auto it:p)
+            {
+                a[it-'a']++;
+            }
+            for(int i=0;i<m;i++)
+            {
+                b[s[i]-'a']++;
+            }
+            if(a==b) ans.push_back(0);
+            for(int i=m;i<n;i++)
+            {
+                b[s[i]-'a']++;
+                b[s[i-m]-'a']--;
+                if(a==b) ans.push_back(i-m+1);
+            }
         }
         return ans;
     }
